7214f64b02e223d0f43bf1a8f7e457d24700e08b
steven-halla <steven.r.halla@gmail.com>
2023-06-24 13:07:09 -0700

    deleted files and home page

---
+++ goodstuff-web/src/Home.tsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return(
+      <div>
+          <p>
+              I'm the Home page homey
+          </p>
+          <a href="/login">Login page</a>
+      </div>
+    );
+}

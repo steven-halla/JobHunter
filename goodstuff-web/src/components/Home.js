55fd4b5afe56ed5894bfb8faf606001f9d06aac9
steven-halla <steven.r.halla@gmail.com>
2023-07-03 12:06:34 -0700

    almost finished with home 1st arc

--- goodstuff-web/src/components/Home.js
+++ goodstuff-web/src/components/Home.js
@@ -1,88 +1,84 @@
 import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 
-
 import UserService from "../services/user.service";
 
 const Home = () => {
-  const [content, setContent] = useState("");
+    const [content, setContent] = useState("");
 
-  useEffect(() => {
-    UserService.getPublicContent().then(
-      (response) => {
-        setContent(response.data);
-      },
-      (error) => {
-        const _content =
-          (error.response && error.response.data) ||
-          error.message ||
-          error.toString();
+    useEffect(() => {
+        UserService.getPublicContent().then(
+            (response) => {
+                setContent(response.data);
+            },
+            (error) => {
+                const _content =
+                    (error.response && error.response.data) ||
+                    error.message ||
+                    error.toString();
 
-        setContent(_content);
-      }
+                setContent(_content);
+            }
+        );
+    }, []);
+
+    const weedtop5 = [
+        {name: "blueberry purple", type: "sativa", descriptionOfHigh: "you'll get so high you'll turn purple",img: "hemped.jpeg"},
+        {name: "rotten haze", type: "indica", descriptionOfHigh: "it'll rot your lungs so dont smoke too much", img: "hemped.jpeg"},
+        {name: "princess cannibal ", type: "hybrid 50/50", descriptionOfHigh: "this weed will melt the flesh off your bones",img: "hemped.jpeg"},
+        {name: "rib demon", type: "hybrid india dominant", descriptionOfHigh: "you'll be so high demons really will exist",img: "hemped.jpeg"},
+        {name: "Mr Blunt", type: "hybrid sativa dominant", descriptionOfHigh: "Mr. GoodBar's brother",img: "hemped.jpeg"},
+    ]
+
+    return (
+        <PinkPageDiv>
+            <HomeBanner>
+                <p>WELCOME TO THE WEED HOME PAGE. DO YOU LIKE WEED? IF NOT, YOU CAN JUST GET OUT OF HERE!</p>
+            </HomeBanner>
+            <WeedTop5List>
+                <li className="header">
+                    <p>Name</p>
+                    <p>Type</p>
+                    <p>Description</p>
+                </li>
+                {weedtop5.map((weed, index) => (
+                    <li key={index}>
+                        <img src={weed.img} alt={weed.name} className="weed-image"/>
+                        <p>{weed.name}</p>
+                        <p>{weed.type}</p>
+                        <p>{weed.descriptionOfHigh}</p>
+                    </li>
+                ))}
+            </WeedTop5List>
+            <HomeFooter>
+                This is the footer
+            </HomeFooter>
+        </PinkPageDiv>
     );
-  }, []);
 
-  const weedtop5 = [
-      {name: "blueberry purple", type: "sativa", descriptionOfHigh: "you'll get so high you'll turn purple"},
-      {name: "rotten haze", type: "indica", descriptionOfHigh: "it'll rot your lungs so dont smoke too much"},
-      {name: "princess cannibal ", type: "hybrid 50/50", descriptionOfHigh: "this weed will melt the flesh off your bones"},
-      {name: "rib demon", type: "hybrid india dominant", descriptionOfHigh: "you'll be so high demons really will exist"},
-      {name: "Mr Blunt", type: "hybrid sativa dominant", descriptionOfHigh: "Mr. GoodBar's brother"},
-  ]
-
-  return (
-      <PinkPageDiv>
-
-          <HomeBanner>
-            <p>WELCOME TO THE WEED HOME PAGE. DO YOU LIKE WEED? IF NOT, YOU CAN JUST GET OUT OF HERE!</p>
-          </HomeBanner>
-          <WeedTop5List>
-              <li>
-                  <div>Name</div>
-                  <div>Type</div>
-                  <div>Description of High</div>
-              </li>
-              {weedtop5.map((weed, index) => (
-                  <li key={index}>
-                      <div>{weed.name}</div>
-                      <div>{weed.type}</div>
-                      <div>{weed.descriptionOfHigh}</div>
-                  </li>
-              ))}
-          </WeedTop5List>
-
-          <HomeFooter>
-              This is the footer
-          </HomeFooter>
-      </PinkPageDiv>
-  );
 };
 
 const PinkPageDiv = styled.div`
-  
   *, *:before, *:after {
     box-sizing: border-box;
   }
   
   background-color: pink;
   display: flex;
-
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   height: 100vh;
   width: 100vw;
-  
 `
 
 const HomeBanner = styled.header`
   display: flex;
-  justify-content: center; // Centers children along the horizontal line
-  align-items: center; // Centers children along the vertical line
-
+  justify-content: center;
+  align-items: center;
   background-color: #baadff;
   height: 10vh;
   width: 80vw;
-  margin-left: auto;
-  margin-right: auto;
   margin-top: 1vh;
 
   p {
@@ -92,15 +88,11 @@ const HomeBanner = styled.header`
 
 const HomeFooter = styled.header`
   display: flex;
-  justify-content: center; // Centers children along the horizontal line
-  align-items: center; // Centers children along the vertical line
-
+  justify-content: center;
+  align-items: center;
   background-color: #deffd2;
   height: 10vh;
   width: 100vw;
-
-  position: fixed; // Fixed positioning
-  bottom: 0; // Position at the bottom
 
   p {
     font-size: 1.5rem;
@@ -108,37 +100,80 @@ const HomeFooter = styled.header`
 `;
 
 const WeedTop5List = styled.ul`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
   display: flex;
-  flex-direction: column; 
-  justify-content: center; 
-  align-items: center; 
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: #bcd8ff;
-  height: 50vh;
-  width: 100vw;
+  height: 60vh;
+  width: 80vw;
+
+  li.header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around; // adjust as needed
+    align-items: flex-start;
+  }
+
+  li:not(.header) {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 20px;
+  }
 
   li {
     display: flex;
     justify-content: space-between;
-    width: 70%;  // You can adjust this value as per your requirements
-    padding: 10px;  // Add some padding to separate the lines
+    align-items: center;
+    gap: 20px;
   }
 
-  li div {
-    width: 33%;
-    text-align: center;  // Make all text centered
-    padding: 0 10px; // Add padding to give some space between the columns
+  li:first-child {
+    justify-content: center;
+  }
+
+  img.weed-image {
+    flex-basis: 100px;
+    height: 60px;
+    object-fit: cover;
+    object-position: 0 -20px;
+  }
+
+  p {
+    flex-basis: 0;
+    flex-grow: 1;
+  }
+
+
+  .weed-image.hidden {
+    visibility: hidden;
+  }
+
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    width: 100%;
+    margin-bottom: 1vh;
+  }
+
+  .weed-image {
+    flex-basis: 10%;
+    height: 30px;  // Adjust this value as needed for your specific images
+    object-fit: cover;
+    object-position: 0 -40px;  // This line "crops" the image, effectively removing 20 pixels from the top
+  }
+
+  div {
+    flex-basis: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
   }
 `;
-
-
-
-
-
-
 
 export default Home;

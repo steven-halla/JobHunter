5a178cb3b053cc8ab440a0890fb1f3bc07431dbf
steven-halla <steven.r.halla@gmail.com>
2023-11-12 10:42:37 -0800

    adding a new method to soft delete

--- jobhunting-service/src/main/java/com/stevenhalla/spring/login/repository/JobsRepository.java
+++ jobhunting-service/src/main/java/com/stevenhalla/spring/login/repository/JobsRepository.java
@@ -1,8 +1,8 @@
 package com.stevenhalla.spring.login.repository;
 
-
 import com.stevenhalla.spring.login.models.Jobs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +11,8 @@ import java.util.List;
 public interface JobsRepository extends JpaRepository<Jobs, Long> {
 
     List<Jobs> findByUserId(Long userId);
+
+    // Adjusted query to include jobs where jobsoftdelete is false or null
+    @Query("SELECT j FROM Jobs j WHERE j.jobsoftdelete IS NULL OR j.jobsoftdelete = false")
+    List<Jobs> findAllActiveJobs();
 }

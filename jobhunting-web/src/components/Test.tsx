9ce54f71a72c727a2650f285adaf514aee6b8f78
steven-halla <steven.r.halla@gmail.com>
2023-11-13 12:52:57 -0800

    test is good to go

--- jobhunting-web/src/components/Test.tsx
+++ jobhunting-web/src/components/Test.tsx
@@ -1,127 +1,86 @@
 import React, { useState, useEffect, useContext } from 'react';
-import Calendar from 'react-calendar';
-import styled from 'styled-components';
-import 'react-calendar/dist/Calendar.css';
-import { JobsContext } from "../services/jobcontext";
-import {InterviewCalendarModal} from "./InterviewCalandarModal";
+import styled from "styled-components";
 
 
-
-type InterviewData = {
-    jobId: number;
-    userId: number;
-    companyname: string;
-    interviewdate: Date | null;
-    interviewernames: string;
-    interviewnotes: string;
-    meetingLink: string;
-};
 
 export const Test = () => {
-    const { jobs } = useContext(JobsContext);
-    const [interviewData, setInterviewData] = useState<InterviewData[]>([]);
-    const [showModal, setShowModal] = useState<boolean>(false);
-    const [modalContent, setModalContent] = useState<React.ReactNode>(null);
 
-    useEffect(() => {
-        const formattedData = jobs.map(job => ({
-            jobId: job.id,
-            userId: job.userid,
-            companyname: job.companyname,
-            interviewdate: (typeof job.interviewdate === 'string' || job.interviewdate instanceof Date)
-                ? new Date(job.interviewdate)
-                : null,
-            interviewernames: typeof job.interviewernames === 'string' ? job.interviewernames : '',
-            interviewnotes: typeof job.interviewnotes === 'string' ? job.interviewnotes : '',
-            meetingLink: typeof job.meetingLink === 'string' ? job.meetingLink : '',
-        }));
+    interface Card {
+        name: string;
+        company: string;
+        number: number;
+        description: string;
+        date: number;
+    }
 
-        setInterviewData(formattedData);
-    }, [jobs]);
-    const getTileContent = ({ date, view }: { date: Date; view: string }) => {
-        if (view === 'month') {
-            const dayInterviews = interviewData.filter(interview =>
-                interview.interviewdate && interview.interviewdate.toDateString() === date.toDateString()
-            );
-            return (
-                <div>
-                    {dayInterviews.map((interview, index) => (
-                        <div key={index}>
-                            <p> {interview.companyname}</p>
-                            {/* Display other interview details as needed */}
-                        </div>
-                    ))}
-                </div>
-            );
+    const cards: Card[] = [
+        {
+            name: "pluto",
+            company: "not a planet",
+            number: 143333939,
+            description: "it's still not a planet, its a satellite dish",
+            date: 14433
+        },
+        {
+            name: "neptune",
+            company: "also not a planet",
+            number: 987654321,
+            description: "neptune is a gas giant",
+            date: 9876
+        },
+        {
+            name: "juno",
+            company: "is a planet",
+            number: 143333939,
+            description: "By the light of the god emperor its a planet",
+            date: 14433
+        },
+        {
+            name: "kravos",
+            company: "orky boys",
+            number: 987654321,
+            description: "noob stompas rule dis ere place",
+            date: 9876
         }
-    };
+    ];
 
-    const onDayClick = (value: Date, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        const dayInterviews = interviewData.filter(interview =>
-            interview.interviewdate && interview.interviewdate.toDateString() === value.toDateString()
-        );
 
-        if (dayInterviews.length > 0) {
-            const modalContentJSX = dayInterviews.map((interview, index) => (
-                <div key={index}>
-                    <p><strong>Company:</strong> {interview.companyname}</p>
-                    <p><strong>Interviewer Names:</strong> {interview.interviewernames}</p>
-                    <p><strong>Interview Notes:</strong> {interview.interviewnotes}</p>
-                    <p><strong>Meeting Link:</strong> <a href={interview.meetingLink} target="_blank" rel="noopener noreferrer">{interview.meetingLink}</a></p>
-                </div>
-            ));
-            setModalContent(modalContentJSX);
-            setShowModal(true);
-        }
-    };
-
-    const getTileClassName = ({ date, view }: { date: Date; view: string }) => {
-        if (view === 'month') {
-            const hasInterview = interviewData.some(interview =>
-                interview.interviewdate && interview.interviewdate.toDateString() === date.toDateString()
-            );
-            return hasInterview ? 'interview-day' : '';
-        }
-    };
 
 
     return (
-        <CalendarContainer>
-            <StyledCalendar
-                tileContent={getTileContent}
-                tileClassName={getTileClassName}
-                onClickDay={onDayClick} />
-            <InterviewCalendarModal show={showModal} onClose={() => setShowModal(false)}>
-                {modalContent}
-            </InterviewCalendarModal>
-        </CalendarContainer>
+        <TestWrapperDiv>
+            <CardBoxDiv>
+                {cards.map((card: Card, index: number) => (
+                    <CardDiv key={index}>
+                        <h2>{card.name}</h2>
+                        <p>Company: {card.company}</p>
+                        <p>Number: {card.number}</p>
+                        <p>Description: {card.description}</p>
+                        <p>Date: {card.date}</p>
+                    </CardDiv>
+                ))}
+            </CardBoxDiv>
+        </TestWrapperDiv>
     );
 };
 
-
-const StyledCalendar = styled(Calendar)`
-  width: 75%;
-  height: 75vh;
-  max-width: 100%;
-  .react-calendar__tile {
-    max-height: 100px; // Adjust as needed
-    overflow: hidden;
-  }
-
-  .interview-day {
-    background-color: lightgreen;
-  }
-
-  /* Add additional styling here if needed */
-`;
-
-
-
-// If needed, create a styled div to center the calendar
-const CalendarContainer = styled.div`
+const TestWrapperDiv = styled.div`
   display: flex;
+  height: 100%;
+  width: 100%;
+  background-color: peachpuff;
   justify-content: center;
   align-items: center;
-  height: 100vh; // This makes sure the container takes the full height of the viewport
 `;
 
+const CardBoxDiv = styled.div`
+  display: flex;
+  flex-direction: column; /* Align cards vertically */
+  gap: 10px; /* Add 10px spacing between cards */
+`;
+
+const CardDiv = styled.div`
+  background-color: white; /* Background color for each card */
+  padding: 10px; /* Add some padding around each card */
+  border: 1px solid #ccc; /* Add a border for each card */
+`;

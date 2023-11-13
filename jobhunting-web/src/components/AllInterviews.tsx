547b9eb10766efc9ca94f8544d40e73799b3c307
steven-halla <steven.r.halla@gmail.com>
2023-11-12 16:09:18 -0800

    now displaying all data for all intervies

--- jobhunting-web/src/components/AllInterviews.tsx
+++ jobhunting-web/src/components/AllInterviews.tsx
@@ -14,12 +14,13 @@ type InterviewData = {
     interviews: Interview[];
 };
 export const AllInterviews = () => {
+
     const { jobs } = useContext(JobsContext);
     const [allInterviewData, setAllInterviewData] = useState<InterviewData[]>([]);
 
     useEffect(() => {
         const aggregatedInterviewData = jobs
-            .filter(job => typeof job.meetingLink === 'string' && job.meetingLink)
+            .filter(job => typeof job.meetingLink === 'string' && job.meetingLink) // Ensure meetingLink is a string and not empty
             .map(job => ({
                 jobId: job.id,
                 userId: job.userid,
@@ -27,43 +28,48 @@ export const AllInterviews = () => {
                 interviewdate: typeof job.interviewdate === 'function' ? null : job.interviewdate,
                 interviewernames: typeof job.interviewernames === 'function' ? '' : job.interviewernames,
                 interviewnotes: typeof job.interviewnotes === 'function' ? '' : job.interviewnotes,
-                meetingLink: job.meetingLink as string,
+                meetingLink: job.meetingLink as string, // Cast to string to satisfy TypeScript
                 interviews: job.interviews
             }));
 
         setAllInterviewData(aggregatedInterviewData);
+
+        console.log("Filtered Interview Data:", aggregatedInterviewData);
     }, [jobs]);
 
+
+    const formatDate = (date: string | number | Date | null) => {
+        if (date instanceof Date) {
+            return date.toLocaleDateString();
+        } else if (date) {
+            // Attempt to parse the date if it's a string or another type
+            try {
+                return new Date(date).toLocaleDateString();
+            } catch (e) {
+                console.error('Error parsing date:', e);
+            }
+        }
+        return 'Date not available';
+    };
 
     return (
         <div>
             <h2>All Interviews</h2>
-            {allInterviewData.length > 0 ? (
-                <ul>
-                    {allInterviewData.map((data, index) => (
-                        <li key={index}>
-                            <p>Job ID: {data.jobId}</p>
-                            <p>Company Name: {data.companyname}</p>
-                            <p>Interview Date: {data.interviewdate instanceof Date
-                                ? DateMutation(data.interviewdate.toISOString())
-                                : "Date not available"}
-                            </p>
-                            <p>Interviewer Names: {data.interviewernames}</p>
-                            <p>Meeting Link: <a href={data.meetingLink} target="_blank" rel="noopener noreferrer">{data.meetingLink}</a></p>
-                            <p>Interview Notes: {data.interviewnotes}</p>
-                            {/* Displaying each interview detail if available */}
-                            {data.interviews && data.interviews.map((interview, i) => (
-                                <div key={i}>
-                                    <p>Interview Time: {interview.interviewtime}</p>
-                                    <p>Interview Secured: {interview.secured ? 'Yes' : 'No'}</p>
-                                </div>
-                            ))}
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No interviews available.</p>
-            )}
+            <ul>
+                {allInterviewData.map((data, index) => (
+                    <li key={index}>
+                        <p>Company Name: {data.companyname}</p>
+                        <p>Interviewer Names: {data.interviewernames}</p>
+                        <p>Interview Notes: {data.interviewnotes}</p>
+                        <p>Meeting Link: <a href={data.meetingLink} target="_blank" rel="noopener noreferrer">{data.meetingLink}</a></p>
+                        <p>Interview Date: {
+                            data.interviewdate
+                                ? DateMutation(typeof data.interviewdate === 'string' ? data.interviewdate : data.interviewdate.toISOString())
+                                : 'Date not available'
+                        }</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
-};
+}

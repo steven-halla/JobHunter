e66699f802be57c183f9fba01d4843db67596e97
steven-halla <steven.r.halla@gmail.com>
2023-11-18 16:10:54 -0800

    can now create jobs again but there is an error with home

--- jobhunting-web/src/components/Home.tsx
+++ jobhunting-web/src/components/Home.tsx
@@ -33,6 +33,7 @@ export const Home: React.FC = () => {
     const [ interviewdate, setInterviewDate] = useState<Date>(new Date("2023-07-22"));
     const [companyresponded, setCompanyResponded] = useState<boolean>(false);
     const [companyrejected, setCompanyRejected] = useState<boolean>(false);
+    const [jobsoftdelete, setJobSoftDelete] = useState<boolean>(false);
     const [selectedOption1, setSelectedOption1] = useState(localStorage.getItem('selectedOption1') || 'github');
     const [selectedOption2, setSelectedOption2] = useState(localStorage.getItem('selectedOption2') || 'default');
     const [selectedOption3, setSelectedOption3] = useState(localStorage.getItem('selectedOption3') || 'portfolio');
@@ -62,6 +63,7 @@ export const Home: React.FC = () => {
         dateapplied: Date;
         companyresponded: boolean;
         companyrejected: boolean;
+        jobsoftdelete: boolean;
     }
 
 
@@ -124,7 +126,7 @@ export const Home: React.FC = () => {
                         },
                         body: JSON.stringify({ companyname, customfield, description, jobposter, primarycontact,
                             companywebsitelink, joblink , interviewnotes,  interviewernames, dateapplied,
-                            interviewdate, companyresponded, companyrejected}),
+                            interviewdate, companyresponded, companyrejected, jobsoftdelete}),
                     }
                 );
                 if (response.ok) {
@@ -143,6 +145,7 @@ export const Home: React.FC = () => {
                     setInterviewDate(new Date());
                     setCompanyResponded(false);
                     setCompanyRejected(false);
+                    setJobSoftDelete(false);
                     setCount(count + 1);
                     alert("Adding +1 to the counter.")
                 } else {
